ca6dfd6692f8df15e74f7392ec54b64f2599f43a
ayush02303 <142813464+ayush02303@users.noreply.github.com>
2025-06-27 02:57:37 +0000

    done

--- basics/second.js
+++ basics/second.js
@@ -7,3 +7,15 @@ console.log(typeof(aaa));
 let a = 0
 let b = Boolean(a); 
 console.log(b);
+
+// string addition
+let str1 = "naming"
+let str2 = " conventiom"
+console.log(str1+str2);
+
+//comparision datatypes
+let num1 = 1233; 
+let num2 = "1233"
+
+console.log( num1 == num2); 
+console.log( num1 === num2);
