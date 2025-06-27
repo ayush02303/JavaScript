a3edda8c1fcb8dbc05992c01a4b63baafb774ce1
ayush02303 <142813464+ayush02303@users.noreply.github.com>
2025-06-27 01:20:51 +0000

    container confid done

--- basics/test.js
+++ basics/test.js
@@ -1,4 +1,20 @@
-let a = 10
-let b = 23; 
-let c = a+b; 
-console.log(c); 
+function groupAnagrams(strs) {
+    const map = new Map();
+
+    for (let str of strs) {
+        // Sort the string to get the key
+        const key = str.split('').sort().join('');
+        // Push into map under that key
+        if (!map.has(key)) {
+            map.set(key, []);
+        }
+        map.get(key).push(str);
+    }
+
+    // Convert map values to array of arrays
+    return Array.from(map.values());
+}
+
+// Test the function
+const input = ["eat", "tea", "tan", "ate", "nat", "bat"];
+console.log(groupAnagrams(input));
