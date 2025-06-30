51c4ae08ce089b763705617de9bb3325d5cc892c
ayush02303 <142813464+ayush02303@users.noreply.github.com>
2025-06-30 07:13:34 +0000

    switch cases

---
+++ intermediate/switch.js
@@ -0,0 +1,20 @@
+console.log("switch cases"); 
+const num = 2
+
+switch(num){
+    case 1:
+        console.log(`you've written ${num}`)
+    break;
+
+    case  2:
+        console.log(`you've written ${num}`)
+    break;
+    case 3 : 
+    console.log(`you've written ${num}`)
+
+     case 4 : 
+    console.log(`you've written ${num}`)
+
+     case 5 : 
+    console.log(`you've written ${num}`)
+}
