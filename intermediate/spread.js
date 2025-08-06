e5fa1bfed3f4d4a6d9a5fbab549165dc8783f3c6
Ayush Badola <142813464+ayush02303@users.noreply.github.com>
2025-08-06 15:54:15 +0000

    spread

---
+++ intermediate/spread.js
@@ -0,0 +1,18 @@
+let numbers = [1,2,3,4,5]; 
+
+let maximum  = Math.max(...numbers)
+console.log(maximum); 
+
+let name = "Hello There People"
+
+
+
+let answer = [...name]
+console.log(answer); 
+
+let food = ["apple", "mango" , "banana"]; 
+let items = [ "pen" , "paper" , "subtle"]; 
+
+let ans = [...food , ...items]
+console.log(ans);
+
