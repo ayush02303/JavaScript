97c79072cfbd7f1d3ed2b64a3f3e32bcf5ce20ea
Ayush Badola <142813464+ayush02303@users.noreply.github.com>
2025-09-30 08:00:34 +0000

    forofloops

---
+++ intermediate/forOf_forInLoops.js
@@ -0,0 +1,13 @@
+// for of loop , provides item present in the array 
+
+let fruits = ["apple" , "mango" ,"orange"]
+
+for( let item of fruits ){
+    console.log(item)
+}a
+
+// for in loop , provides index
+
+for( let item in fruits){
+    console.log( item)
+}
