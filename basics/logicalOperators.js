f337cb44154dcbd268cb60fdd0b1862f81c2bdd6
Ayush Badola <142813464+ayush02303@users.noreply.github.com>
2025-09-30 04:56:51 +0000

    logical operators

---
+++ basics/logicalOperators.js
@@ -0,0 +1,10 @@
+// logical operators include 
+// Logical AND && 
+// Logical OR || 
+// Logical NOT !
+
+let a = true
+let b = false 
+
+if( a && b ) console.log("will not print")
+else if( a || b) console.log("will get printed")
